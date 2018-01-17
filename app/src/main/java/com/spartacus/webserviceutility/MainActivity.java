dcd4d2b0b72a29c23d4e6b65e77b0c78b5f115ee
mohammad <m3bndah@gmail.com>
2018-01-17 18:10:13 +0200

    new

--- app/src/main/java/com/spartacus/webserviceutility/MainActivity.java
+++ app/src/main/java/com/spartacus/webserviceutility/MainActivity.java
@@ -3,6 +3,7 @@ package com.spartacus.webserviceutility;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 
 import com.spartacus.webserviceutility.constant.WSConstants;
@@ -11,6 +12,7 @@ import com.spartacus.webserviceutility.webservice.WSAsync;
 import com.spartacus.webserviceutility.webservice.WSResponse;
 import com.spartacus.webserviceutility.webservice.WSResponseListener;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity implements WSResponseListener {
@@ -31,6 +33,17 @@ public class MainActivity extends AppCompatActivity implements WSResponseListene
             JSONObject jsonObject = new JSONObject();
             jsonObject.put(WSConstants.PARAM_USERNAME, "user");
             jsonObject.put(WSConstants.PARAM_PASSWORD, "pass");
+
+            //send array
+           /* JSONArray jsonArray = new JSONArray();
+            for (String string :iist) {
+                jsonArray.put(string);
+            }
+            jsonObject.put(WSConstants.array, jsonArray);*/
+
+           //send pic
+           // jsonObject.put(WSConstants.PARAM_Pic, Base64.encodeToString(image, 0));
+
             if (Utility.isNetworkAvailable(this.getApplicationContext(), false)) {
                 new WSAsync(this, this, jsonObject, WSConstants.API_URL, WSConstants.CALLID, "CONNECTING_TO_SERVER", false).execute();
             } else {
